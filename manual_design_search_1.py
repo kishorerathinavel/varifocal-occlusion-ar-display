181376aadd4c776d119959d0edb2b2a1a30cdec2
kishorerathinavel <kishore@cs.unc.edu>
2018-12-27 17:06:54 -0800

    GUI has updateable labels, connected sliders-spinboxes

--- manual_design_search_1.py
+++ manual_design_search_1.py
@@ -2,7 +2,7 @@
 
 from PyQt5.QtCore import pyqtSignal, Qt
 from PyQt5.QtWidgets import (QApplication, QGridLayout, QGroupBox,
-                             QVBoxLayout, QSlider, QSpinBox, QWidget)
+                             QVBoxLayout, QSlider, QSpinBox, QWidget, QLabel)
 
 class SlidersGroup(QGroupBox):
 
@@ -22,44 +22,50 @@ class SlidersGroup(QGroupBox):
         self.slider.setTickInterval(10)
         self.slider.setSingleStep(1)
 
-        self.spinBox_b = QSpinBox()
-        self.spinBox_b.setRange(-100, 100)
-        self.spinBox_b.setSingleStep(1)
-
-        self.spinBox_e = QSpinBox()
-        self.spinBox_e.setRange(-100, 100)
-        self.spinBox_e.setSingleStep(1)
-
-        self.spinBox_b.valueChanged.connect(self.setMinimum)
-        self.spinBox_b.setValue(minimumValue)
-        self.spinBox_e.valueChanged.connect(self.setMaximum)
-        self.spinBox_e.setValue(maximumValue)
-
         self.spinBox_c = QSpinBox()
-        self.spinBox_c.setRange(-100, 100)
+        self.spinBox_c.setRange(minimumValue, maximumValue)
         self.spinBox_c.setSingleStep(1)
-
         self.slider.valueChanged.connect(self.spinBox_c.setValue)
         self.spinBox_c.valueChanged.connect(self.slider.setValue)
         self.spinBox_c.setValue(spinBoxValue)
 
-        slidersLayout2 = QGridLayout()
-        slidersLayout2.addWidget(self.spinBox_b, 0, 0, 1, 1)
-        slidersLayout2.addWidget(self.spinBox_e, 0, 3, 1, 1)
-        slidersLayout2.addWidget(self.slider, 1, 0, 1, 4)
-        slidersLayout2.addWidget(self.spinBox_c, 1, 4, 1, 1)
+        self.spinBox_b = QSpinBox()
+        self.spinBox_b.setRange(-100, 100)
+        self.spinBox_b.setSingleStep(1)
+        self.spinBox_b.valueChanged.connect(self.setMinimum)
+        self.spinBox_b.setValue(minimumValue)
 
-        self.setLayout(slidersLayout2)
+        self.spinBox_e = QSpinBox()
+        self.spinBox_e.setRange(-100, 100)
+        self.spinBox_e.setSingleStep(1)
+        self.spinBox_e.valueChanged.connect(self.setMaximum)
+        self.spinBox_e.setValue(maximumValue)
+
+        self.currValueLabel = QLabel(str(spinBoxValue))
+        self.slider.valueChanged.connect(self.updateLabel)
+
+        slidersLayout = QGridLayout()
+        slidersLayout.addWidget(self.spinBox_b, 0, 0, 1, 1)
+        slidersLayout.addWidget(self.spinBox_e, 0, 3, 1, 1)
+        slidersLayout.addWidget(self.slider, 1, 0, 1, 4)
+        slidersLayout.addWidget(self.spinBox_c, 1, 4, 1, 1)
+        slidersLayout.addWidget(self.currValueLabel, 1, 5, 1, 1)
+
+        self.setLayout(slidersLayout)
 
     def setMinimum(self, value):    
         self.slider.setMinimum(value)
+        self.spinBox_c.setMinimum(value)
 
     def setMaximum(self, value):    
         self.slider.setMaximum(value)
+        self.spinBox_c.setMaximum(value)
+
+    def updateLabel(self, value):
+        self.currValueLabel.setText(str(value))
 
 class Window(QWidget):
     def __init__(self):
-        
         super(Window, self).__init__()
         self.slider1 = SlidersGroup("Horizontal1")
         self.slider2 = SlidersGroup("Horizontal2")
@@ -68,6 +74,17 @@ class Window(QWidget):
         layout.addWidget(self.slider2)
         self.setLayout(layout)
         self.setWindowTitle("Sliders")
+
+    def keyReleaseEvent(self, e):
+        if e.key() == Qt.Key_Q:
+            if(self.slider1.spinBox_c.minimum() < self.slider1.spinBox_c.value()):
+                self.slider1.spinBox_c.setValue(self.slider1.spinBox_c.value() - 1)
+        if e.key() == Qt.Key_W:
+            if(self.slider1.spinBox_c.maximum() > self.slider1.spinBox_c.value()):
+                self.slider1.spinBox_c.setValue(self.slider1.spinBox_c.value() + 1)
+        if e.key() == Qt.Key_Escape:
+            self.close()
+
 
 if __name__ == '__main__':
     import sys
