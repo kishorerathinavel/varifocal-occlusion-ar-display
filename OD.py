e9b8f543838f31f8a839f3f09bb6880be69ddf84
kishorerathinavel <kishore@cs.unc.edu>
2019-01-28 13:06:16 -0800

    Uploading progress from last week.

--- OD.py
+++ OD.py
@@ -6,111 +6,58 @@ from numpy import linalg as LA
 Refer to abcd.svg for the diagram of the optical designs modelled here
 '''
 
+class lens():
+    def __init__(self):
+        self.focal_length = 0.0
+        self.d_prev_lens = 0.0
+        self.d_object = 0.0
+        self.d_image = 0.0
+        self.magnification = 0.0
+        self.tunable = False
+        self.M = []
+        self.S = []
+        
+    
 class optical_design(): 
     def __init__(self):
-        self.d_f0_f1 = 4.0
-        self.d_f1_LCoS = 4.0 # Minimum possible
-        self.d_LCoS_f2 = 4.0 # Minimum possible
-        self.d_f1_f2 = self.d_f1_LCoS + self.d_LCoS_f2
-        self.d_f2_f3 = 12 # Guess
-        self.d_f3_f4 = self.d_f1_f2
-        self.d_f4_eye = 2.0
+        self.lens_l = []
+        self.min_d_f_LCoS = 4.0
+        self.min_d_f_eye = 2.0
         self.num_lenses = 0
-        self.include_all = False
-
-        # Uninitialized
-        self.f0 = 10
-        self.f1 = 0.0
-        self.f2 = 0.0
-        self.f3 = 0.0
-        self.f4 = 0.0
-        self.d_W_f1 = 0.0  # World (not necessarily vip) to f1
-        self.d_W_eye = 0.0 # World (not necessarily vip) to eye
-        self.d_WI_eye = 0.0 # Image of world (not necessarily image of world at vip) to eye
-        self.d_OI_eye = 0.0 # Occlusion mask to eye
+        self.num_lenses_om = 0
         self.d_vip_eye = 0.0
         self.magnification = 0.0
 
-    def populate_d_eye(self, curr_dist):
-        self.d_W_eye = curr_dist
-        self.d_W_f1 = self.d_W_eye - self.d_f4_eye - self.d_f3_f4 - self.d_f2_f3 - self.d_f1_f2
-
     def populate_focal_lengths(self, f):
-        self.f1 = f[0]
-        self.f2 = f[1]
-        if(self.num_lenses > 2 or self.include_all == True):
-            self.f3 = f[2]
-        if(self.num_lenses > 3 or self.include_all == True):
-            self.f4 = f[3]
-
-    def old_populate_focal_lengths(self, f):
-        common_f2_f3 = 3.43
-        if(self.num_lenses == 2):
-            self.f1 = f[0]
-            self.f2 = common_f2_f3
-            if(self.num_lenses > 2 or self.include_all == True):
-                self.f3 = common_f2_f3
-            if(self.num_lenses > 3 or self.include_all == True):
-                self.f4 = f[1]
-        elif(self.num_lenses == 3):
-            self.f1 = f[0]
-            self.f2 = f[1]
-            if(self.num_lenses > 2 or self.include_all == True):
-                self.f3 = f[1]
-            if(self.num_lenses > 3 or self.include_all == True):
-                self.f4 = f[2]
+        counter = 0
+        for lens in self.lens_l:
+            if(lens.tunable == True):
+                lens.focal_length = f[counter]
+                counter = counter + 1
 
     def propagate_rw_all(self, curr_dist):
-        self.rw_magnification = 1.0
-        self.O1 = curr_dist
-        self.I1 = cf.calculate_image_distance(self.O1, self.f1)
-        self.m1 = -self.I1/self.O1
-        self.rw_magnification = self.rw_magnification * self.m1
-        self.O2 = self.d_f1_f2 - self.I1
-        self.I2 = cf.calculate_image_distance(self.O2, self.f2)
-        self.final_I = self.I2
-        self.m2 = -self.I2/self.O2
-        self.rw_magnification = self.rw_magnification * self.m2
-        if(self.num_lenses > 2 or self.include_all == True):
-            self.O3 = self.d_f2_f3 - self.I2
-            self.I3 = cf.calculate_image_distance(self.O3, self.f3)
-            self.final_I = self.I3
-            self.m3 = -self.I3/self.O3
-            self.rw_magnification = self.rw_magnification *self.m3
-            if(self.num_lenses > 3 or self.include_all == True):
-                self.O4 = self.d_f3_f4 - self.I3
-                self.I4 = cf.calculate_image_distance(self.O4, self.f4)
-                self.final_I = self.I4
-                self.m4 = -self.I4/self.O4
-                self.rw_magnification = self.rw_magnification *self.m4
-        # self.rw_magnification = self.m1*self.m2*self.m3*self.m4
-        # self.d_WI_f4 = self.I4
-        # self.d_WI_f1 = self.d_WI_f4 + self.d_f1_f2 + self.d_f2_f3 + self.d_f3_f4
+        self.magnification = 1.0
+        for lens in self.lens_l:
+            lens.d_image = cf.calculate_image_distance(lens.d_object, lens.focal_length)
+            lens.magnification = -lens.d_image/lens.d_object
+            self.magnification = self.magnification * lens.magnification
 
     def propagate_om(self):
-        self.O2 = self.d_LCoS_f2
-        self.I2 = cf.calculate_image_distance(self.O2, self.f2)
-        self.final_I = self.I2
-        if(self.num_lenses > 2 or self.include_all):
-            self.O3 = self.d_f2_f3 - self.I2
-            self.I3 = cf.calculate_image_distance(self.O3, self.f3)
-            self.final_I = self.I3
-            if(self.num_lenses > 3 or self.include_all == True):
-                self.O4 = self.d_f3_f4 - self.I3
-                self.I4 = cf.calculate_image_distance(self.O4, self.f4)
-                self.final_I = self.I4
+        self.magnification = 1.0
+        for lens in self.lens_l[self.num_lenses - self.num_lenses_om:]:
+            lens.d_image = cf.calculate_image_distance(lens.d_object, lens.focal_length)
+            lens.magnification = -lens.d_image/lens.d_object
+            self.magnification = self.magnification * lens.magnification
 
     def calc_ABCD_matrices(self):
-        self.M1 = cf.makeLensMatrix(self.f1)
-        self.M2 = cf.makeLensMatrix(self.f2)
-        self.M3 = cf.makeLensMatrix(self.f3)
-        self.M4 = cf.makeLensMatrix(self.f4)
-        self.S12 = cf.makeFreeSpacePropagationMatrix(self.d_f1_f2)
-        self.S23 = cf.makeFreeSpacePropagationMatrix(self.d_f2_f3)
-        self.S34 = cf.makeFreeSpacePropagationMatrix(self.d_f3_f4)
+        for lens in self.lens_l:
+            lens.M = cf.makeLensMatrix(lens.focal_length)
+            lens.S = cf.makeFreeSpacePropagationMatrix(lens.d_prev_lens)
 
     def calc_TA(self):
-            self.TA = self.M4*self.S34*self.M3*self.S23*self.M2*self.S12*self.M1
+        self.TA = Matrix([[1,0], [0,1]])
+        for lens in self.lens_l:
+            self.TA = self.TA * lens.M * lens.S
 
     def prototype_v4_populate_dependent_focalLengths(self):
         self.f3 = self.f2
--- optical_design_optimization.py
+++ optical_design_optimization.py
@@ -7,6 +7,8 @@ from scipy.optimize import NonlinearConstraint
 from scipy.optimize import SR1
 import common_functions as cf
 import OD
+from pprint import pprint
+
 
 '''
 Need to minimize:
@@ -21,27 +23,19 @@ Outpus of energy function:
 norm of the vector of errors mentioned above
 '''
 
-common_f2_f3 = 0.0
-
-def call_populate_focal_lengths(f, IOD):
-    if(IOD.num_lenses == 2):
-        IOD.populate_focal_lengths((f[0], common_f2_f3, common_f2_f3, f[1]))
-    elif(IOD.num_lenses == 3):
-        IOD.populate_focal_lengths((f[0], f[1], f[1], f[2]))
-
 ''' Propagate using Lensmaker's equations '''
 def energy_function(f, IOD): 
     rw_dists_l = [1000.0, 250.0, 64.0, 16.0]
 
-    call_populate_focal_lengths(f, IOD)
+    IOD.populate_focal_lengths(f)
     
     # Error associated with distance and magnitude of a bunch of real world distances
     err_mag_l = []
     err_dist_l = []
     for rw_dist in rw_dists_l:
         IOD.propagate_rw_all(rw_dist)
-        curr_err_dist = cf.convert_cm2dpt(rw_dist) + cf.convert_cm2dpt(IOD.final_I)
-        curr_err_mag = IOD.rw_magnification - 1.0
+        curr_err_dist = cf.convert_cm2dpt(rw_dist) + cf.convert_cm2dpt(IOD.lens_l[-1].d_image)
+        curr_err_mag = IOD.magnification - 1.0
         err_dist_l.append(curr_err_dist)
         err_mag_l.append(curr_err_mag)
     err_dist_arr = np.squeeze(np.array(err_dist_l))
@@ -51,25 +45,54 @@ def energy_function(f, IOD):
 
     # Error associated with distance at which the occlusion mask is seen
     IOD.propagate_om()
-    curr_err_om = cf.convert_cm2dpt(IOD.final_I) + cf.convert_cm2dpt(IOD.d_vip_eye)
+    curr_err_om = cf.convert_cm2dpt(IOD.lens_l[-1].d_image) + cf.convert_cm2dpt(IOD.d_vip_eye)
     energy_om = curr_err_om**2
 
     combined_energy = energy_dist + energy_mag + 100*energy_om
     return combined_energy
 
-def using_differential_evolution():
-    global common_f2_f3
-
-    show_fl_in_diopters = True
-
-    IOD = OD.optical_design()
-    IOD.num_lenses = 2
-    IOD.include_all = True
-    dists_l = [1000.0, 250.0, 64.0, 16.0]
-
+def initialize_IOD(IOD):
     common_f2_f3 = 3.5
-    IOD.d_f2_f3 = (2*IOD.d_f1_f2*common_f2_f3)/(IOD.d_f1_f2 - common_f2_f3)
-    print('d_f2_f2: %7.2f' % (IOD.d_f2_f3))
+
+    # Lens 1
+    curr_lens = OD.lens()
+    curr_lens.focal_length = -1
+    curr_lens.d_prev_lens = 0.0
+    curr_lens.tunable = True
+    IOD.lens_l.append(curr_lens)
+    
+    # Lens 2
+    curr_lens = OD.lens()
+    curr_lens.focal_length = common_f2_f3
+    curr_lens.d_prev_lens = 2*IOD.min_d_f_LCoS
+    curr_lens.tunable = False
+    IOD.lens_l.append(curr_lens)
+
+    # Lens 3
+    curr_lens = OD.lens()
+    curr_lens.focal_length = common_f2_f3
+    d_f2_f3 = (2*IOD.lens_l[1].d_prev_lens*common_f2_f3)/(IOD.lens_l[1].d_prev_lens - common_f2_f3)
+    print('d_f2_f2: %7.2f' % (d_f2_f3))
+    curr_lens.d_prev_lens = d_f2_f3
+    curr_lens.tunable = False
+    IOD.lens_l.append(curr_lens)
+
+    # Lens 4
+    curr_lens = OD.lens()
+    curr_lens.focal_length = -1
+    curr_lens.d_prev_lens = 2*IOD.min_d_f_LCoS
+    curr_lens.tunable = True
+    IOD.lens_l.append(curr_lens)
+
+    IOD.num_lenses = len(IOD.lens_l)
+    IOD.num_lenses_om = IOD.num_lenses - 1
+
+def using_differential_evolution():
+    show_fl_in_diopters = True
+    IOD = OD.optical_design()
+    initialize_IOD(IOD)
+
+    dists_l = [1000.0, 250.0, 64.0, 16.0]
 
     '''  Focal power range for optotune lenses
          |             | min(cm) | max(cm) | min(D) | max(D) | comments  |
@@ -78,7 +101,7 @@ def using_differential_evolution():
     '''
     myBounds = []
     if(IOD.num_lenses == 2):
-        # myBounds = [(2.0, 4.0), (0.0, 800.0)]
+        myBounds = [(2.0, 4.0), (0.0, 800.0)]
         # myBounds = [(10.0, 20.0), (10.0, 20.0)] # Assuming EL-10-30-C
         # myBounds = [(5.0, 12.0), (5.0, 12.0)] # Assuming EL-10-30-TC
     elif(IOD.num_lenses == 3):
@@ -87,17 +110,19 @@ def using_differential_evolution():
         myBounds = [(0.0, 4.0), (0.0, 10.0), (0.0, 10.0), (-50.0, 50.0)]
     energy_function_args = [IOD]
 
+    for lens in IOD.lens_l:
+        pprint(vars(lens))
+    pprint(vars(IOD))
+
     print('     OM      RW        f1      f2      f3      f4       ~RW    ~MAG     ~OM    ~RW0')
     if(show_fl_in_diopters == True):
         print('   (cm)    (cm)     (dpt)   (dpt)   (dpt)   (dpt)')
     else:
         print('   (cm)    (cm)      (cm)    (cm)    (cm)    (cm)')
     # print('1000.00 1000.00 |    3.98    4.00    4.00    4.02 |   -0.00    0.00   -0.00   -0.00')
+
     for vip_dist in dists_l:
         IOD.d_vip_eye = vip_dist
-
-        # IOD.populate_d_eye(curr_dist)
-        # IOD.d_W_f1 = IOD.d_vip_eye
 
         curr_res = differential_evolution(energy_function, bounds=myBounds, args=energy_function_args)
         res = curr_res
@@ -109,21 +134,24 @@ def using_differential_evolution():
                 min_energy = curr_res.fun
                 res = curr_res
 
-        call_populate_focal_lengths(res.x, IOD)
+        IOD.populate_focal_lengths(res.x)
         for rw_dist in dists_l:
             IOD.propagate_rw_all(rw_dist)
-            curr_err_dist = cf.convert_cm2dpt(rw_dist) + cf.convert_cm2dpt(IOD.final_I)
-            curr_err_mag = IOD.rw_magnification - 1.0
+            curr_err_dist = cf.convert_cm2dpt(rw_dist) + cf.convert_cm2dpt(IOD.lens_l[-1].d_image)
+            curr_err_mag = IOD.magnification - 1.0
             IOD.propagate_rw_all(vip_dist)
-            curr_err_infocus_rw = cf.convert_cm2dpt(IOD.I1) - cf.convert_cm2dpt(IOD.d_f1_LCoS)
+            curr_err_infocus_rw = cf.convert_cm2dpt(IOD.lens_l[0].d_image) - cf.convert_cm2dpt(IOD.min_d_f_LCoS)
             IOD.propagate_om()
-            curr_err_om = cf.convert_cm2dpt(IOD.final_I) + cf.convert_cm2dpt(IOD.d_vip_eye)
+            curr_err_om = cf.convert_cm2dpt(IOD.lens_l[-1].d_image) + cf.convert_cm2dpt(IOD.d_vip_eye)
 
-
+            f1 = IOD.lens_l[0].focal_length
+            f2 = IOD.lens_l[1].focal_length
+            f3 = IOD.lens_l[2].focal_length
+            f4 = IOD.lens_l[3].focal_length
             if(show_fl_in_diopters == True):
-                print('%7.2f %7.2f | %7.2f %7.2f %7.2f %7.2f | %7.2f %7.2f %7.2f %7.2f'% (vip_dist, rw_dist, cf.convert_cm2dpt(IOD.f1), cf.convert_cm2dpt(IOD.f2), cf.convert_cm2dpt(IOD.f3), cf.convert_cm2dpt(IOD.f4), curr_err_dist, curr_err_mag, curr_err_om, curr_err_infocus_rw))
+                print('%7.2f %7.2f | %7.2f %7.2f %7.2f %7.2f | %7.2f %7.2f %7.2f %7.2f'% (vip_dist, rw_dist, cf.convert_cm2dpt(f1), cf.convert_cm2dpt(f2), cf.convert_cm2dpt(f3), cf.convert_cm2dpt(f4), curr_err_dist, curr_err_mag, curr_err_om, curr_err_infocus_rw))
             else:
-                print('%7.2f %7.2f | %7.2f %7.2f %7.2f %7.2f | %7.2f %7.2f %7.2f %7.2f'% (vip_dist, rw_dist, IOD.f1, IOD.f2, IOD.f3, IOD.f4, curr_err_dist, curr_err_mag, curr_err_om, curr_err_infocus_rw))
+                print('%7.2f %7.2f | %7.2f %7.2f %7.2f %7.2f | %7.2f %7.2f %7.2f %7.2f'% (vip_dist, rw_dist, f1, f2, f3, f4, curr_err_dist, curr_err_mag, curr_err_om, curr_err_infocus_rw))
 
 if __name__ == '__main__':
     using_differential_evolution()
