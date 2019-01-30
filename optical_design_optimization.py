6182391998f6e3fbb66fd8cd64abc539b7be992a
kishorerathinavel <kishore@cs.unc.edu>
2019-01-29 18:54:16 -0800

    Syncing to continue working from home

--- optical_design_optimization.py
+++ optical_design_optimization.py
@@ -25,7 +25,7 @@ norm of the vector of errors mentioned above
 ''' Propagate using Lensmaker's equations '''
 
 
-dists_l = [1000.0, 250.0, 64.0, 20.0]
+dists_l = [1000.0, 250.0, 64.0, 17.0]
 
 def energy_function(f, IOD): 
 
@@ -55,12 +55,16 @@ def energy_function(f, IOD):
 
 def initialize_IOD(IOD):
     common_f2_f3 = 3.5
-    half_width_35mm_lens = 2.65
+    half_width_35mm_lens = 1.75
     # half_width_35mm_lens = 1.00
     d_f1_f2 = 2*IOD.min_d_f_LCoS + half_width_35mm_lens
     # d_f3_f4 = d_f1_F2 # Has to be symmetrical
     d_f3_f4 = 2*IOD.min_d_f_LCoS + half_width_35mm_lens
-    d_f2_f3 = (2*d_f3_f4*common_f2_f3)/(d_f3_f4 - common_f2_f3)
+    d_f2_f3 = (2*d_f1_f2*common_f2_f3)/(d_f1_f2 - common_f2_f3)
+    min_d_f2_f3 = 10.7 - 2*half_width_35mm_lens
+    if(d_f2_f3 < min_d_f2_f3):
+        print('d_f2_f3 can not be smaller than %f' %(min_d_f2_f3))
+        d_f2_f3 = min_d_f2_f3
 
     # Lens 1
     curr_lens = OD.lens()
@@ -100,7 +104,7 @@ def initialize_IOD(IOD):
             
 
 def using_differential_evolution():
-    show_fl_in_diopters = True
+    show_fl_in_diopters = False
     IOD = OD.optical_design()
     initialize_IOD(IOD)
 
@@ -112,7 +116,7 @@ def using_differential_evolution():
     '''
     # myBounds = []
     if(IOD.num_tunable_lenses == 2):
-        myBounds = [(2.0, 4.0), (0.0, 90.0)]
+        myBounds = [(2.0, 4.0), (-10.0, 90.0)]
         # myBounds = [(10.0, 20.0), (10.0, 20.0)] # Assuming EL-10-30-C
         # myBounds = [(5.0, 12.0), (5.0, 12.0)] # Assuming EL-10-30-TC
     elif(IOD.num_tunable_lenses == 3):
@@ -125,11 +129,12 @@ def using_differential_evolution():
         pprint(vars(lens))
     pprint(vars(IOD))
 
-    print('     OM      RW        f1      f2      f3      f4       ~RW    ~MAG     ~OM    ~RW0')
     if(show_fl_in_diopters == True):
-        print('   (cm)    (cm)     (dpt)   (dpt)   (dpt)   (dpt)')
+        print('     OM      RW        f1      f2      f3      f4       ~RW    ~MAG     ~OM  ')
+        print('   (cm)    (cm)     (dpt)   (dpt)   (dpt)   (dpt)     (dpt)           (dpt)')
     else:
-        print('   (cm)    (cm)      (cm)    (cm)    (cm)    (cm)')
+        print('     OM      RW        f1      f2      f3      f4       ~RW    ~MAG     ~OM  ')
+        print('   (cm)    (cm)      (cm)    (cm)    (cm)    (cm)      (cm)            (cm)')
     # print('1000.00 1000.00 |    3.98    4.00    4.00    4.02 |   -0.00    0.00   -0.00   -0.00')
 
     f1_l = []
@@ -150,23 +155,30 @@ def using_differential_evolution():
         IOD.populate_focal_lengths(res.x)
         for rw_dist in dists_l:
             IOD.propagate_rw_all(rw_dist)
-            curr_err_dist = cf.convert_cm2dpt(rw_dist) + cf.convert_cm2dpt(IOD.lens_l[-1].d_image)
+            if(show_fl_in_diopters):
+                curr_err_dist = cf.convert_cm2dpt(rw_dist) + cf.convert_cm2dpt(IOD.lens_l[-1].d_image)
+            else:
+                curr_err_dist = rw_dist + IOD.lens_l[-1].d_image
+
             curr_err_mag = IOD.magnification - 1.0
-            IOD.propagate_rw_all(vip_dist)
-            curr_err_infocus_rw = cf.convert_cm2dpt(IOD.lens_l[0].d_image) - cf.convert_cm2dpt(IOD.min_d_f_LCoS)
+            
             IOD.propagate_om()
-            curr_err_om = cf.convert_cm2dpt(IOD.lens_l[-1].d_image) + cf.convert_cm2dpt(IOD.d_vip_eye)
+            if(show_fl_in_diopters):
+                curr_err_om = cf.convert_cm2dpt(IOD.lens_l[-1].d_image) + cf.convert_cm2dpt(IOD.d_vip_eye)
+            else:
+                curr_err_om = IOD.lens_l[-1].d_image + IOD.d_vip_eye
 
             f1 = IOD.lens_l[0].focal_length
             f2 = IOD.lens_l[1].focal_length
             f3 = IOD.lens_l[2].focal_length
             f4 = IOD.lens_l[3].focal_length
+            if(show_fl_in_diopters == True):
+                print('%7.2f %7.2f | %7.2f %7.2f %7.2f %7.2f | %7.2f %7.2f %7.2f'% (vip_dist, rw_dist, cf.convert_cm2dpt(f1), cf.convert_cm2dpt(f2), cf.convert_cm2dpt(f3), cf.convert_cm2dpt(f4), curr_err_dist, curr_err_mag, curr_err_om))
+            else:
+                print('%7.2f %7.2f | %7.2f %7.2f %7.2f %7.2f | %7.2f %7.2f %7.2f'% (vip_dist, rw_dist, f1, f2, f3, f4, curr_err_dist, curr_err_mag, curr_err_om))
+
             f1_l.append(cf.convert_cm2dpt(f1))
             f4_l.append(cf.convert_cm2dpt(f4))
-            if(show_fl_in_diopters == True):
-                print('%7.2f %7.2f | %7.2f %7.2f %7.2f %7.2f | %7.2f %7.2f %7.2f %7.2f'% (vip_dist, rw_dist, cf.convert_cm2dpt(f1), cf.convert_cm2dpt(f2), cf.convert_cm2dpt(f3), cf.convert_cm2dpt(f4), curr_err_dist, curr_err_mag, curr_err_om, curr_err_infocus_rw))
-            else:
-                print('%7.2f %7.2f | %7.2f %7.2f %7.2f %7.2f | %7.2f %7.2f %7.2f %7.2f'% (vip_dist, rw_dist, f1, f2, f3, f4, curr_err_dist, curr_err_mag, curr_err_om, curr_err_infocus_rw))
 
     print(np.mean(f1_l))
     print(np.mean(f1_l) - 7.5)
